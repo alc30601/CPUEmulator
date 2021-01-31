b1ad10581a924183142b0bee11067934a3af89a4
alc30601 <alc30601@bca.bai.ne.jp>
2021-01-31 10:21:46 +0900

    updated

--- Executor.hpp
+++ Executor.hpp
@@ -2,13 +2,30 @@
 #ifndef __EXECUTOR_HPP__
 #define __EXECUTOR_HPP__
 
+#include <memory>
+
+#include "Node.hpp"
+
+
 //-----------------------------------------------------------
 // Start node of the graph
 class Executor
 {
+    std::shared_ptr<Node> _startNode;
 
 public:
     //-------------------------------------------------------
+    Executor(std::shared_ptr<Node> sNode)
+    {
+        _startNode = sNode;
+    }
+
+    //-------------------------------------------------------
+    // execute flow graph from start node
+    void step(void)
+    {
+
+    }
 
 };
 
@@ -16,3 +33,4 @@ public:
 
 
 
+ 
