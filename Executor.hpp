c4adb5a049674dd3bb7ca6b2cece4032c13ac97c
alc30601 <alc30601@bca.bai.ne.jp>
2021-01-31 10:29:24 +0900

    updated

--- Executor.hpp
+++ Executor.hpp
@@ -24,7 +24,15 @@ public:
     // execute flow graph from start node
     void step(void)
     {
+        // ★★★
+        // メンバ変数として持つ、_startNodeから始めてグラフをたどる。
+        // あるNodeからその先に行くにはNodeクラスのgetOutputEdge()によりつながるEdgeを取得する。
+        // あるEdgeからその先のNodeを見つけるにはEdgeのメソッドgetoutputNode()によりNode一覧を取得する。
 
+        // 個々のノードに到達したらそのノードの処理を実行する(execute())。
+        // 但し、到達したNodeにまだデータが到達せずに処理が実行できない場合がある。
+        // その判定のため、そのNodeに入力されるEdge一覧を取得し、全てのEdgeでデータが到達しているか
+        // をチェックする。
     }
 
 };
--- Node.hpp
+++ Node.hpp
@@ -8,14 +8,14 @@ class Node
     // Execute the operation of this node
     virtual void execute(void) = 0;
 
-
+    // ★★★
     // 保持するEdge一覧を返すメソッド
     // 個々のNodeにおけるEdgeの持ち方は様々なため、
     // 本クラスでは純仮想関数とし、継承先のクラスで実装することとする。
     // 汎用メソッドとするにはEdgeの型も共通にしないといけないが、
     // 保持するデータ毎に対応するためにテンプレートを使うと難しいかもしれない。
     // テンプレートが使えないならば、継承で持たせる？
-    // virtual List<std::shread_ptr<Edge>> getOutputEdge();
+    // virtual List<std::shread_ptr<Edge>> ｖ();
 
 };
 
